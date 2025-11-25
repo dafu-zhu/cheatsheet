c516f6f53360dd4fecc61a1eeef718921651ba5c
Dafu <dafuzhu@uchicago.edu>
2025-11-24 22:27:33 -0600

    edit config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // For GitHub Pages deployment, uncomment and set the base path:
-  // base: '/your-repo-name/',
+  base: '/cheatsheet/',
 })
